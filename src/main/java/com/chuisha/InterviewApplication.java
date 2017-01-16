7fb9ef671c76ae84be8b8474d7342827f7c46f0d
xifeng <chenxf10@126.com>
2017-01-16 21:10:47 +0800

    初始提交

---
+++ src/main/java/com/chuisha/InterviewApplication.java
@@ -0,0 +1,48 @@
+package com.chuisha;
+
+import com.chuisha.util.ChuishaMapper;
+import org.beetl.core.resource.ClasspathResourceLoader;
+import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
+import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.chuisha.interview.mapper", markerInterface = ChuishaMapper.class)
+public class InterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewApplication.class, args);
+    }
+
+    @Bean(initMethod = "init", name = "beetlConfig")
+    public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
+
+        BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
+        ResourcePatternResolver patternResolver = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader());
+        ClasspathResourceLoader cploder = new ClasspathResourceLoader("/templates");
+        beetlGroupUtilConfiguration.setResourceLoader(cploder);
+
+        //读取配置文件信息
+        beetlGroupUtilConfiguration.setConfigFileResource(patternResolver.getResource("classpath:beetl.properties"));
+        return beetlGroupUtilConfiguration;
+    }
+
+    @Bean(name = "beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(@Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+        BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
+        beetlSpringViewResolver.setPrefix("/");
+        beetlSpringViewResolver.setSuffix(".html");
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
+        beetlSpringViewResolver.setOrder(0);
+        beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
+        return beetlSpringViewResolver;
+    }
+
+}
